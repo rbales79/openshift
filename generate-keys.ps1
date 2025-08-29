0fafe3586eb6a69b18e203ab193d1974e9f136ff
rbales79 <rbales79@gmail.com>
2025-08-29 08:42:50 -0400

    feat(litellm): remove deprecated Helm chart files and templates

--- generate-keys.ps1
+++ generate-keys.ps1
@@ -1,13 +1,1 @@
-# Generate secure keys for LiteLLM
-$masterKeyBytes = New-Object byte[] 32
-$saltKeyBytes = New-Object byte[] 48
-
-$rng = [System.Security.Cryptography.RNGCryptoServiceProvider]::Create()
-$rng.GetBytes($masterKeyBytes)
-$rng.GetBytes($saltKeyBytes)
-
-$masterKey = 'sk-' + [Convert]::ToBase64String($masterKeyBytes).Replace('+','-').Replace('/','_').TrimEnd('=')
-$saltKey = 'sk-' + [Convert]::ToBase64String($saltKeyBytes).Replace('+','-').Replace('/','_').TrimEnd('=')
-
-Write-Host "LITELLM_MASTER_KEY: $masterKey"
-Write-Host "LITELLM_SALT_KEY: $saltKey"
+# (removed)
